51dbf82a1dbfd399a3d9900e055b68e56166c7b5
cgomezlop95 <c.gomezlop95@gmail.com>
2023-12-02 19:52:47 +0100

    more adjustments

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const postsRoutes = require('./posts');
-router.use('/', postsRoutes);
+const postsRoutes = require("./posts");
+router.use("/posts", postsRoutes);
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -2,90 +2,6 @@ const express = require("express");
 const router = express.Router();
 const prisma = require("../prisma");
 
-//GET - /posts - Crear la ruta para obtener todos los posts
-//Crear la vista para mostrar todos los posts
-
-router.get("/posts", async (req, res) => {
-  try {
-    const posts = await prisma.post.findMany();
-    //res.json(posts);
-    res.render("home", { title: "Posts", posts: posts });
-  } catch (error) {
-    console.error(error);
-    res.json("Server error");
-  }
-});
-
-//POST - /posts - Crear la ruta para crear un post
-
-router.post("/posts", async (req, res) => {
-  try {
-    const newPost = await prisma.post.create({
-      data: {
-        title: req.body.title,
-        content: req.body.content,
-        published: req.body.published,
-      },
-    });
-    res.json(newPost);
-    console.log("Creating one post");
-  } catch (error) {
-    console.error(error);
-    res.json("Server error");
-  }
-});
-
-//GET - /posts/:id - Crear la ruta para obtener un post por su id
-//Crear la vista para mostrar un post
-
-router.get("/posts/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const postById = await prisma.post.findUnique({
-      where: {
-        id,
-      },
-    });
-    //res.json(postById);
-    res.render("post", { title: postById.title, post: postById });
-    console.log("Getting one post by ID");
-  } catch (error) {
-    console.error(error);
-    res.json("Server error");
-  }
-});
-
-//PUT - /posts/:id - Crear la ruta para actualizar un post por su id
-
-router.put("/posts/:id", async (req, res) => {
-  try {
-    const updatedPost = await prisma.post.update({
-      where: { id: req.params.id },
-      data: { title: req.body.title },
-    });
-    res.json(updatedPost);
-    console.log("Updating one post by ID");
-  } catch (error) {
-    console.error(error);
-    res.json("Server error");
-  }
-});
-
-//DELETE - /posts/:id - Crear la ruta para eliminar un post por su id
-
-router.delete("/posts/:id", async (req, res) => {
-  try {
-    const deletedPost = await prisma.post.delete({
-      where: { id: req.params.id },
-    });
-    res.json(deletedPost);
-    console.log("Deleting one post by ID");
-  } catch (error) {
-    console.error(error);
-    res.json("Server error");
-  }
-});
-
 // Crear la vista para crear y actualizar un post, con un formulario
 
 router.get("/create", async (req, res) => {
@@ -109,6 +25,89 @@ router.post("/create", async (req, res) => {
       },
     });
     res.redirect("/posts");
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//GET - /posts - Crear la ruta para obtener todos los posts
+//Crear la vista para mostrar todos los posts
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await prisma.post.findMany();
+    //res.json(posts);
+    res.render("home", { title: "Posts", posts: posts });
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//POST - /posts - Crear la ruta para crear un post
+
+router.post("/", async (req, res) => {
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        title: req.body.title,
+        content: req.body.content,
+        published: req.body.published,
+      },
+    });
+    res.json(newPost);
+    console.log("Creating one post");
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//GET - /posts/:id - Crear la ruta para obtener un post por su id
+//Crear la vista para mostrar un post
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const postById = await prisma.post.findUnique({
+      where: {
+        id,
+      },
+    });
+    res.render("post", { title: postById.title, post: postById });
+    console.log("Getting one post by ID");
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//PUT - /posts/:id - Crear la ruta para actualizar un post por su id
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedPost = await prisma.post.update({
+      where: { id: req.params.id },
+      data: { title: req.body.title },
+    });
+    res.json(updatedPost);
+    console.log("Updating one post by ID");
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//DELETE - /posts/:id - Crear la ruta para eliminar un post por su id
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedPost = await prisma.post.delete({
+      where: { id: req.params.id },
+    });
+    res.json(deletedPost);
+    console.log("Deleting one post by ID");
   } catch (error) {
     console.error(error);
     res.json("Server error");
