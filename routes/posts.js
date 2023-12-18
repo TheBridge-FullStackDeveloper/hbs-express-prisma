b977ba77b2891022a121fe7286039e8a6c240b0b
ricardoyp <ricardoyp07@gmail.com>
2023-12-18 13:14:43 +0100

    "Styles"

--- routes/posts.js
+++ routes/posts.js
@@ -19,25 +19,6 @@ router.get('/', isAuthenticated, async (req, res) => {
     }
 });
 
-// Display All Posts from User
-// router.get('/profile', isAuthenticated, async (req, res) => {
-//     try {
-//         const userId = req.user.id;
-//         const user = await prisma.user.findUnique({
-//             where: {
-//                 id: userId,
-//             },
-//             include: {
-//                 posts: true,
-//             },
-//         });
-//         res.render('profile', { title: 'Posts', user: user});
-//     } catch (e) {
-//         console.log(e);
-//         res.json('Server error');
-//     }
-// });
-
 // Display the post creation form
 router.get('/create', isAuthenticated, async (req, res) => {
     try {
@@ -66,85 +47,5 @@ router.post('/create', isAuthenticated, async (req, res) => {
         res.json('Server error');
     }
 });
-
-// Update a post
-router.put('/update/:id', isAuthenticated, async (req, res) => {
-    try {
-        const { title, content } = req.body;
-        const post = await prisma.post.findUnique({
-            where: {
-                id: req.params.id
-            }
-        })
-        if(post.authorId === req.user.id){
-            const post = await prisma.post.update({
-                where: {
-                    id: req.params.id,
-                },
-                data: {
-                    title: req.body.title,
-                    content: req.body.content,
-                },
-            });
-        }
-        res.redirect('/posts');
-    } catch (e) {
-        console.log(e);
-        res.json('Server error');
-    }
-});
-
-// Delete a post
-router.delete('/delete/:id', isAuthenticated, async (req, res) => {
-    try {
-        const post = await prisma.post.findUnique({
-            where: {
-                id: req.params.id
-            }
-        })
-        if(post.authorId === req.user.id){
-        await prisma.post.delete({
-            where: {
-                id: req.params.id,
-            },
-        });
-        }
-        res.redirect('/posts');
-    } catch (e) {
-        console.log(e);
-        res.json('Server error');
-    }
-});
-
-// Display the post update form
-router.get('/update/:id',isAuthenticated, async (req, res) => {
-    try {
-        const post = await prisma.post.findUnique({
-            where: {
-                id: req.params.id,
-            },
-        });
-        res.render('updateForm', { title: 'Update', post: post });
-    } catch (e) {
-        console.log(e);
-        res.json('Server error');
-    }
-});
-
-// Display a single post
-router.get('/:id', isAuthenticated, async (req, res) => {
-    try {
-        const post = await prisma.post.findUnique({
-            where: {
-                id: req.params.id,
-            },
-        });
-        res.render('post', { title: 'Post', post: post });
-    } catch (e) {
-        console.log(e);
-        res.json('Server error');
-    }
-});
-
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const prisma = require('../prisma/client');
 const isAuthenticated = require('../middleware/isAuthenticated');
 
+// Display Posts from User
 router.get('/', isAuthenticated, async (req, res) => {
     try {
         const userId = req.user.id;
@@ -15,13 +16,16 @@ router.get('/', isAuthenticated, async (req, res) => {
                 posts: true,
             },
         });         
-        res.render('profile', { title: 'Posts', user: user});
-    } catch (e) {        console.log(e);
+        res.render('profile', { title: 'Profile', user: user});
+    } catch (e) {
+        console.log(e);
         res.json('Server error');
     }
 });
-router.get('/update', isAuthenticated, async (req, res) => {
+// Display updateProfileForm 
+router.get('/updateProfile', isAuthenticated, async (req, res) => {
     try {
+        console.log(req.user.id)
         const user = await prisma.user.findUnique({
             where: {
                 id: req.user.id,
@@ -34,7 +38,8 @@ router.get('/update', isAuthenticated, async (req, res) => {
     }
 });
 
-router.put('/update', isAuthenticated, async (req, res) => {
+// Update Profile
+router.put('/updateProfile', isAuthenticated, async (req, res) => {
     try {
         const { email } = req.body;
         await prisma.user.update({
@@ -51,7 +56,98 @@ router.put('/update', isAuthenticated, async (req, res) => {
         res.json('Server error');
     }
 });
+// Display one post
+router.get('/:id', isAuthenticated, async (req, res) => {
+    try {
+        const post = await prisma.post.findUnique({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.render('post', { title: 'Post', post: post });
+    } catch (e) {
+        console.log(e);
+        res.json('Server error');
+    }
+});
 
+// Display updateForm for Posts
+router.get('/update/:id',isAuthenticated, async (req, res) => {
+    try {
+        const post = await prisma.post.findUnique({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.render('updateForm', { title: 'Update', post: post });
+    } catch (e) {
+        console.log(e);
+        res.json('Server error');
+    }
+});
 
+// Update posts
+router.put('/update/:id', isAuthenticated, async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        const post = await prisma.post.findUnique({
+            where: {
+                id: req.params.id
+            }
+        })
+        if(post.authorId === req.user.id){
+            const post = await prisma.post.update({
+                where: {
+                    id: req.params.id,
+                },
+                data: {
+                    title: req.body.title,
+                    content: req.body.content,
+                },
+            });
+        }
+        res.redirect('/posts');
+    } catch (e) {
+        console.log(e);
+        res.json('Server error');
+    }
+});
 
+// Delete posts
+router.delete('/delete/:id', isAuthenticated, async (req, res) => {
+    try {
+        const post = await prisma.post.findUnique({
+            where: {
+                id: req.params.id
+            }
+        })
+        if(post.authorId === req.user.id){
+        await prisma.post.delete({
+            where: {
+                id: req.params.id,
+            },
+        });
+        }
+        res.redirect('/posts');
+    } catch (e) {
+        console.log(e);
+        res.json('Server error');
+    }
+});
+
+//  Delete Profile
+router.delete('/delete', isAuthenticated, async (req, res) => {
+    try{
+        await prisma.user.delete({
+            where: {
+                id: req.user.id
+            }
+        })
+        res.redirect('/auth/login');
+    } catch (e) {
+        console.log(e);
+        res.json('Server error');
+    }
+
+})
 module.exports = router;
