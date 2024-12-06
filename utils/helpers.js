7029eaa7dde6a6c3fa15ad28486f582dcac6b33f
PabloValdazoDEV <157512231+PabloValdazoDEV@users.noreply.github.com>
2024-12-06 19:22:59 +0100

    Bonus Hecho???

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,16 @@
-// utils/helpers.js
-
 module.exports = {
     capitalize: function(text) {
         return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
     },
     formatDate: function(date, format) {
-        // Implementación para formatear una fecha
+        const newDate = new Date(date)
+        if(format === "DD/MM/YYYY"){
+            const DD = newDate.getDay() + 1
+            const MM = newDate.getMonth() + 1
+            const YYYY = newDate.getFullYear()
+            return `${DD}/${MM}/${YYYY}`
+        }else{
+            return date
+        }
     }
-    // Puedes añadir más helpers aquí
 };
